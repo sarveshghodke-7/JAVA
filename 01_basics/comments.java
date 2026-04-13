6487fa0dce9ae348b94697b2a2dff29ab4595651
Sarvesh Ghodke <sarvesh07ghodke@gmail.com>
2026-04-13 20:38:04 +0530

    Comments in Java

---
+++ 01_basics/comments.java
@@ -0,0 +1,12 @@
+public class comments {
+    public static void main(String[] args){
+        // This is a single line comment
+
+        /*
+         * This is a multi-line comment
+         * It can span multiple lines
+         */
+
+        System.out.println("Comments are important for code readability!");
+    }
+}
