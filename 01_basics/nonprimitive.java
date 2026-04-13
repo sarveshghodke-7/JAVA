8011ae37217e6f3286989527f7ca2b941669fc4e
Sarvesh Ghodke <sarvesh07ghodke@gmail.com>
2026-04-13 22:30:12 +0530

    Types of Datatypes in Java

---
+++ 01_basics/nonprimitive.java
@@ -0,0 +1,28 @@
+public class nonprimitive {
+    public static void main(String[] args){
+        // Non-primitive data types are also known as reference data types. They are used to store complex data and objects. 
+        // Here are some common non-primitive data types in Java:
+        // 1. String: It is used to store a sequence of characters. It is a class in Java and provides various methods to manipulate strings.
+        String name = "Hello, World!";
+        System.out.println(name);
+
+        // 2. Arrays: They are used to store multiple values of the same type in a single variable. 
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers[0]); // Output: 1
+
+        // 3. Classes and Objects: A class is a blueprint for creating objects. An object is an instance of a class. 
+        // You can define your own classes and create objects from them.
+
+        // Primitive data types in Java include byte, short, int, long, float, double, char, and boolean.
+        // Non-primitive data types include String, Arrays, Classes, and Objects.
+
+        // The main difference between primitive and non-primitive data types is that primitive data types store actual values, 
+        // while non-primitive data types store references to objects.
+        // Non-primitive data types can have methods and properties, while primitive data types do not have any methods or properties.
+        // Non-primitive data types can be null, while primitive data types cannot be null.
+
+        // Primtive data types are stored in the stack memory, while non-primitive data types are stored in the heap memory.
+
+        // Non-primitive data types are not predefined in Java, you can create your own non-primitive data types by defining classes.
+    }    
+}
